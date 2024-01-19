ff363fb28eb1d1b63cb89ed79d3752fbe83f38c9
nixalpha <n1xalphaa@gmail.com>
2024-01-18 18:05:10 -0800

    Fixed css

--- app/asr.js
+++ app/asr.js
@@ -1,9 +1,4 @@
-import {
-  View,
-  Text,
-  ScrollView,
-  TouchableOpacity
-} from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import { useState } from "react";
 
 import { Audio } from "expo-av";
@@ -20,83 +15,91 @@ export default function Page() {
   const [text, setText] = useState("");
 
   return (
-    <View style={{ flex: 1, gap: 20, padding: 16, backgroundColor: '#88A4D3', alignItems: 'center'}}>
-        <TouchableOpacity
-          style={{
-            borderWidth: 1,
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            alignItems: "center",
-            verticalAlign: "middle",
-          }}
-          onPress={async () => {
-            if (permissionResponse.status !== 'granted') {
-              console.log('Requesting permission..');
-              await requestPermission();
+    <View
+      style={{
+        flex: 1,
+        gap: 20,
+        padding: 16,
+        backgroundColor: "#88A4D3",
+        alignItems: "center",
+      }}
+    >
+      <TouchableOpacity
+        style={{
+          borderWidth: 1,
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          alignItems: "center",
+          verticalAlign: "middle",
+        }}
+        onPress={async () => {
+          if (permissionResponse.status !== "granted") {
+            console.log("Requesting permission..");
+            await requestPermission();
+          }
+
+          const ctx = await initWhisper({
+            filePath: require("../assets/ggml-tiny.en.bin"),
+          });
+
+          console.log("Start realtime transcribing...");
+          setIsMicOn(true);
+
+          const options = {
+            language: "en",
+            realtimeAudioSec: 120,
+            realtimeAudioSliceSec: 10,
+          };
+          const { stop, subscribe } = await ctx.transcribeRealtime(options);
+
+          subscribe((evt) => {
+            const { isCapturing, data, processTime, recordingTime } = evt;
+            // console.log(
+            //   `Realtime transcribing: ${isCapturing ? "ON" : "OFF"}\n` +
+            //     // The inference text result from audio record:
+            //     `Result: ${data.result}\n\n` +
+            //     `Process time: ${processTime}ms\n` +
+            //     `Recording time: ${recordingTime}ms`
+            // );
+            console.log(`${data.result}\n\n`);
+            setText(data.result);
+            if (!isCapturing) {
+              console.log("Finished realtime transcribing");
+              setIsMicOn(false);
             }
+          });
+        }}
+      >
+        <MICnedc width={100} height={100} />
+      </TouchableOpacity>
 
-            const ctx = await initWhisper({
-              filePath: require("../assets/ggml-tiny.en.bin"),
-            });
+      <TouchableOpacity
+        style={{
+          width: 150,
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          backgroundColor: "white",
+        }}
+        onPress={() => setIsMicOn(!isMicOn)}
+        disabled={true}
+      >
+        <Text>{isMicOn ? "mic on" : "mic off"}</Text>
+      </TouchableOpacity>
 
-            console.log("Start realtime transcribing...");
-            setIsMicOn(true);
-
-            const options = { 
-              language: 'en',
-              realtimeAudioSec: 120,
-              realtimeAudioSliceSec: 10,
-           };
-            const { stop, subscribe } = await ctx.transcribeRealtime(options);
-
-            subscribe((evt) => {
-              const { isCapturing, data, processTime, recordingTime } = evt;
-              // console.log(
-              //   `Realtime transcribing: ${isCapturing ? "ON" : "OFF"}\n` +
-              //     // The inference text result from audio record:
-              //     `Result: ${data.result}\n\n` +
-              //     `Process time: ${processTime}ms\n` +
-              //     `Recording time: ${recordingTime}ms`
-              // );
-              console.log(`${data.result}\n\n`);
-              setText(data.result);
-              if (!isCapturing) {
-                console.log("Finished realtime transcribing");
-                setIsMicOn(false);
-              }
-            });
-          }}
-        >
-          <MICnedc width={100} height={100} />
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={{
-            width: 150,
-            height: 30,
-            borderRadius: 10,
-            alignItems: "center",
-            backgroundColor: "white"
-          }}
-          onPress={() => setIsMicOn(!isMicOn)}
-          disabled={true}
-        >
-          <Text>{isMicOn ? "mic on" : "mic off"}</Text>
-        </TouchableOpacity>
-
-        <View
-          style={{
-            alignSelf: 'stretch',
-            flex: 1,
-            backgroundColor: "#e1e4f3",
-            borderRadius: 10
-          }}
-        >
-          <ScrollView>
-            <Text>{text}</Text>
-          </ScrollView>
-        </View>
+      <View
+        style={{
+          flex: 1,
+          alignSelf: "stretch",
+          backgroundColor: "#e1e4f3",
+          borderRadius: 10,
+        }}
+      >
+        <ScrollView>
+          <Text>{text}</Text>
+        </ScrollView>
+      </View>
     </View>
   );
 }
--- app/tts.js
+++ app/tts.js
@@ -22,42 +22,39 @@ export default function Page() {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <View
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        padding: 16,
+        gap: 20,
+        backgroundColor: "#88A4D3",
+      }}
+    >
+      <TouchableOpacity
         style={{
-          flex: 1,
+          borderWidth: 1,
+          width: 100,
+          height: 100,
+          borderRadius: 50,
           alignItems: "center",
-          padding: 16,
-          gap: 20,
-          flexDirection: "column",
-          backgroundColor: '#88A4D3'
+          verticalAlign: "middle",
         }}
+        onPress={speak}
       >
-        <TouchableOpacity
-          style={{
-            borderWidth: 1,
-            width: 100,
-            height: 100,
-            borderRadius: 50,
-            alignItems: "center",
-            verticalAlign: "middle",
-          }}
-          onPress={speak}
-        >
-          <Soundbox width={100} height={100} />
-        </TouchableOpacity>
-        <TextInput
-          style={{
-            backgroundColor: "#e1e4f3",
-            height: "75%",
-            width: "100%",
-            borderRadius: 10,
-            textAlignVertical: 'top',
-          }}
-          value={text}
-          onChangeText={setText}
-        ></TextInput>
-      </View>
+        <Soundbox width={100} height={100} />
+      </TouchableOpacity>
+      <TextInput
+        style={{
+          backgroundColor: "#e1e4f3",
+          flex: 1,
+          alignSelf: "stretch",
+          borderRadius: 10,
+          textAlignVertical: "top",
+        }}
+        value={text}
+        onChangeText={setText}
+      ></TextInput>
     </View>
   );
 }
